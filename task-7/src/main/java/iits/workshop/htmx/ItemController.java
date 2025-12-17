dd5d8312d33fdf5f4b5ed4d05f8c133fecd57e86
Frederik Pietzko <frederik.pietzko@iits-consulting.de>
2025-12-17 21:55:38 +0100

    task 7 solution

--- task-7/src/main/java/iits/workshop/htmx/ItemController.java
+++ task-7/src/main/java/iits/workshop/htmx/ItemController.java
@@ -8,16 +8,27 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.servlet.view.FragmentsRendering;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Controller
 public class ItemController {
 
     private final ItemService itemService;
+    private final SpringTemplateEngine templateEngine;
+    private final CopyOnWriteArrayList<SseEmitter> emitters = new CopyOnWriteArrayList<>();
 
-    public ItemController(ItemService itemService) {
+    public ItemController(ItemService itemService, SpringTemplateEngine templateEngine) {
         this.itemService = itemService;
+        this.templateEngine = templateEngine;
     }
 
     @GetMapping("/")
@@ -44,7 +55,8 @@ public class ItemController {
             return "index";
         }
 
-        itemService.createItem(item);
+        Item createdItem = itemService.createItem(item);
+        notifyClients(createdItem);
 
         if (isHtmxRequest) {
             model.addAttribute("item", new Item());
@@ -52,5 +64,33 @@ public class ItemController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping("/items/stream")
+    public SseEmitter streamItems() {
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        emitters.add(emitter);
+
+        emitter.onCompletion(() -> emitters.remove(emitter));
+        emitter.onTimeout(() -> emitters.remove(emitter));
+        emitter.onError((e) -> emitters.remove(emitter));
+
+        return emitter;
+    }
+
+    private void notifyClients(Item item) {
+        Context context = new Context();
+        context.setVariable("item", item);
+        final var html = templateEngine.process("fragments/row-fragment", Set.of("row"), context);
+
+        List<SseEmitter> deadEmitters = new CopyOnWriteArrayList<>();
+        emitters.forEach(emitter -> {
+            try {
+                emitter.send(html);
+            } catch (IOException e) {
+                deadEmitters.add(emitter);
+            }
+        });
+        emitters.removeAll(deadEmitters);
     }
 }
